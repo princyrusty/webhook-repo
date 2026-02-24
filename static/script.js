4c00541534cd120c7e29267a5677938f5a0ae4ab
Princy Rastogi <138395494+princyrusty@users.noreply.github.com>
2026-02-25 03:09:51 +0530

    Change event log display element from 'events' to 'log'
    Replace event list display from 'events' to 'log'.

--- static/script.js
+++ static/script.js
@@ -1,27 +1,23 @@
 async function loadEvents() {
   const res = await fetch("/api/events");
   const data = await res.json();
-
-  const ul = document.getElementById("events");
-  ul.innerHTML = "";
+  const log = document.getElementById("log");
+  log.innerHTML = "";
 
   data.forEach(e => {
     const date = new Date(e.timestamp).toUTCString();
     let text = "";
 
-    if (e.action === "PUSH") {
+    if (e.action === "PUSH")
       text = `${e.author} pushed to ${e.to_branch} on ${date}`;
-    } 
-    else if (e.action === "PULL_REQUEST") {
+    else if (e.action === "PULL_REQUEST")
       text = `${e.author} submitted a pull request from ${e.from_branch} to ${e.to_branch} on ${date}`;
-    } 
-    else if (e.action === "MERGE") {
+    else if (e.action === "MERGE")
       text = `${e.author} merged branch ${e.from_branch} to ${e.to_branch} on ${date}`;
-    }
 
     const li = document.createElement("li");
     li.innerText = text;
-    ul.appendChild(li);
+    log.appendChild(li);
   });
 }
 
